a1df5d4dab816ca0f604a6e179c213feafefbbcc
niels win10 <n@osomi.net>
2019-08-07 22:34:15 +0200

    Updated to new armulator

--- include/gba/emulator.hpp
+++ include/gba/emulator.hpp
@@ -12,16 +12,16 @@ namespace gba {
 		//Extended rom is at 0F000000 -> 7FFF0000 (1807 MiB; 1 GiB + 783 MiB)
 		Emulator(const Buffer &bios, const Buffer &rom, const Buffer &erom):
 			Armulator({
-				{ 0x00000000_u32, u32(bios.size()),	false, "BIOS", "Basic I/O system", bios },
-				{ 0x02000000_u32, u32(256_KiB),		true,  "WRAM", "Work memory (on board)", {} },
-				{ 0x03000000_u32, u32(32_KiB),		true,  "IRAM", "Work memory (in chip)", {}  },
-				{ 0x04000000_u32, u32(0x3FF),		true,  "I/O", "Input/Output registers", {} },
-				{ 0x05000000_u32, u32(1_KiB),		true,  "BG/OBJ RAM", "Palette memory", {} },
-				{ 0x06000000_u32, u32(96_KiB),		true,  "VRAM", "Video memory", {} },
-				{ 0x07000000_u32, u32(1_KiB),		true,  "OAM", "OBJ attributes", {} },
-				{ 0x08000000_u32, u32(rom.size()),	false, "ROM", "Readonly memory", rom },
-				{ 0x0E000000_u32, u32(64_KiB),		true,  "SRAM", "Save memory", {} },
-				{ 0x0F000000_u32, u32(erom.size()),	false, "EROM", "Extended readonly memory", erom }
+				Memory::Range{ 0x00000000_u32,	u32(bios.size()),	false, "BIOS", "Basic I/O system", bios },
+				Memory::Range{ 0x02000000_u32,	u32(256_KiB),		true,  "WRAM", "Work memory (on board)", {} },
+				Memory::Range{ 0x03000000_u32,	u32(32_KiB),		true,  "IRAM", "Work memory (in chip)", {}  },
+				Memory::Range{ 0x04000000_u32,	u32(0x3FF),			true,  "I/O", "Input/Output registers", {} },
+				Memory::Range{ 0x05000000_u32,	u32(1_KiB),			true,  "BG/OBJ RAM", "Palette memory", {} },
+				Memory::Range{ 0x06000000_u32,	u32(96_KiB),		true,  "VRAM", "Video memory", {} },
+				Memory::Range{ 0x07000000_u32,	u32(1_KiB),			true,  "OAM", "OBJ attributes", {} },
+				Memory::Range{ 0x08000000_u32,	u32(rom.size()),	false, "ROM", "Readonly memory", rom },
+				Memory::Range{ 0x0E000000_u32,	u32(64_KiB),		true,  "SRAM", "Save memory", {} },
+				Memory::Range{ 0x0F000000_u32,	u32(erom.size()),	false, "EROM", "Extended readonly memory", erom }
 			}) {}
 
 		~Emulator() = default;
